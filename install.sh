ab5cfb4761ebc689083598368fbe7b3c9f5ba60d
groteck <groteck@gmail.com>
2014-03-15 19:32:28 +0000

    change repo in install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
-    git clone https://github.com/albertohm/dotfiles.git "$HOME/.yadr"
+    git clone git@github.com:groteck/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" == "ask" ] && export ASK="true"
     rake install
